948538124e54f1a031b71ba9d3e49e5730b5bf72
Rosario <rosariodavi9@gmail.com>
2022-08-17 13:47:37 -0400

    fixed guess my birthday

--- game.py
+++ game.py
@@ -1,18 +1,18 @@
 from random import randint
 
 player_name = input("Hi what is your name?")
-guess_number = 1
-month_number = randint(1,12)
-year_number = randint(1942,2004)
 
 for guess_number in range(1,6):
-    month_number
-    year_number
+    month_number = randint(1,12)
+    year_number = randint(1942, 2004)
     print("Guess:", player_name, "were you born in", month_number, "/", year_number, "?")
 
     respone = input("yes or no? ")
 
     if respone == "yes":
         print("I knew it!")
+        exit()
+    elif guess_number == 5:
+        print("goodbye")
     else:
         print("Drat! Lemme try again!")
